1af54147dc909cb81eda910d065c6eae241d3e4f
ibabkov <i.babkov.dev@gmail.com>
2024-06-19 00:10:28 -0400

    feat: update lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'next/core-web-vitals',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
