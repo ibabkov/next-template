c1e6e173fcf4eeb70731c39c1e76bfe338962d31
ibabkov <i.babkov.dev@gmail.com>
2024-06-19 00:06:10 -0400

    fix: fix lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,47 +7,6 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    'next/core-web-vitals',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
-  env: {
-    node: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/no-explicit-any': 0,
-    semi: ['error', 'always'],
-    'comma-dangle': ['error', 'always-multiline'],
-    'newline-before-return': 'error',
-    '@typescript-eslint/no-unused-vars': 'warn',
-    'import/order': [
-      'error',
-      {
-        'newlines-between': 'always',
-        groups: ['builtin', 'external', 'internal'],
-        pathGroups: [
-          {
-            pattern: 'react',
-            group: 'external',
-            position: 'before',
-          },
-        ],
-        pathGroupsExcludedImportTypes: ['react'],
-      },
-    ],
-  },
-};
-module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
