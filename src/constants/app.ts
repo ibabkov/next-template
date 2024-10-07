59b944d70fa3c96389729d3670db454313536b44
ibabkov <i.babkov.dev@gmail.com>
2024-10-07 17:38:22 -0400

    chore: update dependencies

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,8 +1,8 @@
-import { TLanguage } from '../types/language';
+import { Language } from '../types/language';
 
 // TODO Change DISPLAY_APP_TITLE, APP_TITLE, APP_DESCRIPTION
 export const DISPLAY_APP_TITLE = 'DISPLAY_APP_TITLE';
 export const APP_TITLE = 'APP_TITLE';
 export const APP_DESCRIPTION = 'APP_DESCRIPTION';
 
-export const LANGUAGE: TLanguage = 'en';
+export const LANGUAGE: Language = 'en';
